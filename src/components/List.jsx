b785fded380480a6afb0f707c8fdc0dcc9655b16
byp28 <yannight28@gmail.com>
2024-09-16 06:24:25 +0100

    16/09

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,32 +6,74 @@ export default function List() {
 
   const [query,setQuery] = useState("random")
   const [pageNumber, setPageNumber] = useState(1)
-
+  const lastPicRef = useRef()
+  const searchRef = useRef()
   const photosApiData = usePhotos(query,pageNumber)
   console.log(photosApiData)
+
+  useEffect(()=>{
+
+    if(lastPicRef.current){
+      const observer = new IntersectionObserver(([entry])=>{
+        if(entry.isIntersecting && photosApiData.maxPages !== pageNumber){
+          setPageNumber(pageNumber => pageNumber + 1)
+          lastPicRef.current = null
+          observer.disconnect()
+        }
+      })
+      observer.observe(lastPicRef.current)
+    }
+  },[photosApiData])
+
+  function handleSubmit(e){
+    e.preventDefault()
+    if(searchRef.current.value !== query){
+      setQuery(searchRef.current.value)
+      setPageNumber(1)
+    }
+  }
 
   return (
     <>
       <h1 className='text-4xl'>Unsplash Clone.</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label className='block mb-4' htmlFor="search">Look for images...</label>
         <input 
+        ref={searchRef}
         className='block w-full mb-14 text-slate-800 py-3 px-2 text-md outline-gray-500 rounded border-slate-400'
         placeholder ="Look for something" type="text" />
       </form> 
+
+      {photosApiData.error.state && <p>{photosApiData.error.msg}</p>}
+      {(photosApiData.photos.length === 0 && !photosApiData.error.state && !photosApiData.loading) && <p>"No image available for this query"</p>}
+
       <ul className='grid grid-cols-[repeat(auto-fill,minmax(250px,_1fr))] auto-rows-[175px] gap-4 justify-center'>
-        {!photosApiData.loading && photosApiData.photos.length !==0 && photosApiData.photos.map((photo,id)=>(
-          <li key={photo.id}>
-            <img 
-            className='w-full h-full object-cover'
-            src={photo.urls.regular} 
-            alt={photo.alt_description} />
-          </li>
-        ))}
+        {!photosApiData.loading && photosApiData.photos.length !==0 && photosApiData.photos.map((photo,index)=>{
+          if(photosApiData.photos.length === index+1){
+            return(
+              <li ref={lastPicRef} key={photo.id}>
+                <img 
+                className='w-full h-full object-cover'
+                src={photo.urls.regular} 
+                alt={photo.alt_description} />
+              </li>
+            )
+          }else{
+            return(
+              <li key={photo.id}>
+                <img 
+                className='w-full h-full object-cover'
+                src={photo.urls.regular} 
+                alt={photo.alt_description} />
+              </li>
+            )
+          }
+          
+        })}
       </ul>
 
       {/* Loader */}
-      {(photosApiData.loading && !photosApiData.error.state) && (<img className='block mx-auto' src={spinner}/>)}
+      {(photosApiData.loading && !photosApiData.error.state) && <img className='block mx-auto' src={spinner}/>}
     </>
   )
 }
--- src/hooks/usePhotos.js
+++ src/hooks/usePhotos.js
@@ -11,11 +11,28 @@ export default function usePhotos(querySearch,pageIndex) {
   const [loading, setLoading] = useState(true)
 
   useEffect(()=>{
+    if(photos.length !==0 && maxPages !==0){
+      setPhotos([])
+      setMaxPages(0)
+    }
+  }, [querySearch])
+  useEffect(()=>{
+    //setLoading(true)
     fetch(`https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${querySearch}&client_id=${import.meta.env.VITE_UNSPLASH_KEY}`)
-    .then(response => response.json())
+    .then(response => {
+      if(!response.ok) throw new Error(`${response.status} Error, something went wrong`)
+      return response.json()
+    })
     .then(data => {
-      setPhotos(data.results)
+      setPhotos(state => [...state, ...data.results])
       setMaxPages(data.total_pages)
+      setLoading(false)
+    })
+    .catch(err =>{
+      setError({
+        msg : err.message,
+        state : true
+      })
       setLoading(false)
     })
   }, [querySearch, pageIndex])
